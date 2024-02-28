5b3bccf42e7bf622cedc525fce84fd11e01ef91d
AdiGalvan <133288217+AdiGalvan@users.noreply.github.com>
2024-02-27 19:27:18 -0600

    Instalación vite
    Se reaizó la instalación de vite para no tener que ejecutar un comando npx cada que se agrega una calse

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"], //se dice a tailwind que trabaje con todos los archivos que tengan css
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
